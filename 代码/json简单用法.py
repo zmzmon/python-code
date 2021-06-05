6d778486875ed4167ea013e5deab948bd7640131
石安安 <39561473+saa1028@users.noreply.github.com>
2021-06-05 14:14:32 +0800

    Update json简单用法.py

--- 代码/json简单用法.py
+++ 代码/json简单用法.py
@@ -5,7 +5,7 @@ headers = {
                   'Chrome/88.0.4324.182 Mobile Safari/537.36 '
 }
 cookies = {'ASP.NET_SessionId': '5o21lf55ohhulveflzr0zdav'}
-url = 'http://8.135.30.80:8888/admin/user/users.aspx?page=6&action=edit&params=364558'
+url = 'http://0.0.0.0:8888/admin/user/users.aspx?page=6&action=edit&params=364558'
 response = requests.get(url, cookies=cookies)
 f = open("1.html", 'w', encoding='UTF-8')
 f.write(response.content.decode('UTF-8'))
