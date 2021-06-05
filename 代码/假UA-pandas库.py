3ebf9931d22d647330909add0ab6024d3d3a40fa
石安安 <39561473+saa1028@users.noreply.github.com>
2021-06-05 14:15:38 +0800

    Update 假UA-pandas库.py

--- 代码/假UA-pandas库.py
+++ 代码/假UA-pandas库.py
@@ -8,7 +8,7 @@ ua = fake_useragent.UserAgent()
 cookies = {'JSESSIONID': '854DB37C43E3FD0DDDE2CFEDDFF81E50'}
 for i in range(64860):
     headers = {'User-Agent': ua.random}
-    url = 'http://sy.youmobeng.cn/BullPoker/adm/listAgentScoreRecord?currentPage=' + str(i) + '&ownerId=&costType=&startDate=&endDate='
+    url = 'http://XXXX.cn/BullPoker/adm/listAgentScoreRecord?currentPage=' + str(i) + '&ownerId=&costType=&startDate=&endDate='
     response = requests.get(url, headers=headers, cookies=cookies)
     f = open(str(i) + ".html", 'w', encoding='UTF-8')
     f.write(response.content.decode('UTF-8'))
